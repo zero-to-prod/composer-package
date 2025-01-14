b81b9f166aeeca60db8adc5012194243955e69e7
david_smith <david_smith@sweetwater.com>
2025-01-14 12:44:50 -0500

    feat/composer-package Created `src/PearRepository.php`.

---
+++ src/PearRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zerotoprod\ComposerPackage;
+
+use Zerotoprod\DataModel\DataModel;
+use Zerotoprod\DataModel\Describe;
+
+class PearRepository
+{
+    use DataModel;
+
+    /** @see $type */
+    public const type = 'type';
+    /** @see $url */
+    public const url = 'url';
+    /** @see $canonical */
+    public const canonical = 'canonical';
+    /** @see $only */
+    public const only = 'only';
+    /** @see $exclude */
+    public const exclude = 'exclude';
+    /** @see $vendor_alias */
+    public const vendor_alias = 'vendor-alias';
+    public TypeEnum $type;
+    public string $url;
+    public bool $canonical;
+    public array $only;
+    public array $exclude;
+    #[Describe(['from' => self::vendor_alias])]
+    public string $vendor_alias;
+}
