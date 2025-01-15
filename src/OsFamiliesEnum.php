779117dc90a67a369eb6aa345d27a6d8a5cf2ed7
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:33 -0500

    feat/composer-package Updated `src/OsFamiliesEnum.php`.

--- src/OsFamiliesEnum.php
+++ src/OsFamiliesEnum.php
@@ -5,10 +5,16 @@ namespace Zerotoprod\ComposerPackage;
 /** The name of the OS family to mark as compatible. */
 enum OsFamiliesEnum: string
 {
+    /** The name of the OS family to mark as compatible. */
     case windows = 'windows';
+    /** The name of the OS family to mark as compatible. */
     case bsd = 'bsd';
+    /** The name of the OS family to mark as compatible. */
     case darwin = 'darwin';
+    /** The name of the OS family to mark as compatible. */
     case solaris = 'solaris';
+    /** The name of the OS family to mark as compatible. */
     case linux = 'linux';
+    /** The name of the OS family to mark as compatible. */
     case unknown = 'unknown';
 }
