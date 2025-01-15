c0b6c91f8f165acecffb719757032e1b53593f3b
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:33 -0500

    feat/composer-package Updated `src/GitlabProtocolEnum.php`.

--- src/GitlabProtocolEnum.php
+++ src/GitlabProtocolEnum.php
@@ -5,7 +5,10 @@ namespace Zerotoprod\ComposerPackage;
 /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
 enum GitlabProtocolEnum: string
 {
+    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
     case git = 'git';
+    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
     case http = 'http';
+    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
     case https = 'https';
 }
