d1b525813ae719d396cb1efbdd4c259819c7521e
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:32 -0500

    feat/composer-package Updated `src/BinCompatEnum.php`.

--- src/BinCompatEnum.php
+++ src/BinCompatEnum.php
@@ -5,8 +5,12 @@ namespace Zerotoprod\ComposerPackage;
 /** The compatibility of the binaries, defaults to "auto" (automatically guessed), can be "full" (compatible with both Windows and Unix-based systems) and "proxy" (only bash-style proxy). */
 enum BinCompatEnum: string
 {
+    /** The compatibility of the binaries, defaults to "auto" (automatically guessed), can be "full" (compatible with both Windows and Unix-based systems) and "proxy" (only bash-style proxy). */
     case auto = 'auto';
+    /** The compatibility of the binaries, defaults to "auto" (automatically guessed), can be "full" (compatible with both Windows and Unix-based systems) and "proxy" (only bash-style proxy). */
     case full = 'full';
+    /** The compatibility of the binaries, defaults to "auto" (automatically guessed), can be "full" (compatible with both Windows and Unix-based systems) and "proxy" (only bash-style proxy). */
     case proxy = 'proxy';
+    /** The compatibility of the binaries, defaults to "auto" (automatically guessed), can be "full" (compatible with both Windows and Unix-based systems) and "proxy" (only bash-style proxy). */
     case symlink = 'symlink';
 }
