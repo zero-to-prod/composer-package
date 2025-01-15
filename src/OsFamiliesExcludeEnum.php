59760f0e29b0733a35046f43e5e40d859b56d965
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:33 -0500

    feat/composer-package Updated `src/OsFamiliesExcludeEnum.php`.

--- src/OsFamiliesExcludeEnum.php
+++ src/OsFamiliesExcludeEnum.php
@@ -5,10 +5,16 @@ namespace Zerotoprod\ComposerPackage;
 /** The name of the OS family to exclude. */
 enum OsFamiliesExcludeEnum: string
 {
+    /** The name of the OS family to exclude. */
     case windows = 'windows';
+    /** The name of the OS family to exclude. */
     case bsd = 'bsd';
+    /** The name of the OS family to exclude. */
     case darwin = 'darwin';
+    /** The name of the OS family to exclude. */
     case solaris = 'solaris';
+    /** The name of the OS family to exclude. */
     case linux = 'linux';
+    /** The name of the OS family to exclude. */
     case unknown = 'unknown';
 }
