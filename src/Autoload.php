a936c6531f046777f3be71d779559e7b00cc9740
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:32 -0500

    feat/composer-package Updated `src/Autoload.php`.

--- src/Autoload.php
+++ src/Autoload.php
@@ -43,34 +43,34 @@ class Autoload
     /**
      * This is an object of namespaces (keys) and the directories they can be found in (values, can be arrays of paths) by the autoloader.
      *
-     * @var array
+     * @var null|array
      */
     #[Describe(['from' => self::psr_0])]
-    public array $psr_0;
+    public null|array $psr_0 = null;
     /**
      * This is an object of namespaces (keys) and the PSR-4 directories they can map to (values, can be arrays of paths) by the autoloader.
      *
-     * @var array
+     * @var null|array
      */
     #[Describe(['from' => self::psr_4])]
-    public array $psr_4;
+    public null|array $psr_4 = null;
     /**
      * This is an array of paths that contain classes to be included in the class-map generation process.
      *
-     * @var array
+     * @var null|array
      */
-    public array $classmap;
+    public null|array $classmap = null;
     /**
      * This is an array of files that are always required on every request.
      *
-     * @var array
+     * @var null|array
      */
-    public array $files;
+    public null|array $files = null;
     /**
      * This is an array of patterns to exclude from autoload classmap generation. (e.g. "exclude-from-classmap": ["/test/", "/tests/", "/Tests/"]
      *
-     * @var array
+     * @var null|array
      */
     #[Describe(['from' => self::exclude_from_classmap])]
-    public array $exclude_from_classmap;
+    public null|array $exclude_from_classmap = null;
 }
