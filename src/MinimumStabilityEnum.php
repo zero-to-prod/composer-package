02df57de335fc540de28696292e306e1bdae4b83
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:33 -0500

    feat/composer-package Updated `src/MinimumStabilityEnum.php`.

--- src/MinimumStabilityEnum.php
+++ src/MinimumStabilityEnum.php
@@ -5,10 +5,16 @@ namespace Zerotoprod\ComposerPackage;
 /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
 enum MinimumStabilityEnum: string
 {
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case dev = 'dev';
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case alpha = 'alpha';
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case beta = 'beta';
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case rc = 'rc';
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case RC = 'RC';
+    /** The minimum stability the packages must have to be install-able. Possible values are: dev, alpha, beta, RC, stable. */
     case stable = 'stable';
 }
