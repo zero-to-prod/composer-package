937d11aa1043f8db8578515c59f561f8cc7bf6d5
david_smith <david_smith@sweetwater.com>
2025-01-14 21:26:33 -0500

    feat/composer-package Updated `src/Scripts.php`.

--- src/Scripts.php
+++ src/Scripts.php
@@ -108,50 +108,50 @@ class Scripts
     public const post_create_project_cmd = 'post-create-project-cmd';
     /** Occurs before the install command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_install_cmd])]
-    public array|string $pre_install_cmd;
+    public null|array|string $pre_install_cmd = null;
     /** Occurs after the install command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_install_cmd])]
-    public array|string $post_install_cmd;
+    public null|array|string $post_install_cmd = null;
     /** Occurs before the update command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_update_cmd])]
-    public array|string $pre_update_cmd;
+    public null|array|string $pre_update_cmd = null;
     /** Occurs after the update command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_update_cmd])]
-    public array|string $post_update_cmd;
+    public null|array|string $post_update_cmd = null;
     /** Occurs before the status command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_status_cmd])]
-    public array|string $pre_status_cmd;
+    public null|array|string $pre_status_cmd = null;
     /** Occurs after the status command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_status_cmd])]
-    public array|string $post_status_cmd;
+    public null|array|string $post_status_cmd = null;
     /** Occurs before a package is installed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_package_install])]
-    public array|string $pre_package_install;
+    public null|array|string $pre_package_install = null;
     /** Occurs after a package is installed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_package_install])]
-    public array|string $post_package_install;
+    public null|array|string $post_package_install = null;
     /** Occurs before a package is updated, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_package_update])]
-    public array|string $pre_package_update;
+    public null|array|string $pre_package_update = null;
     /** Occurs after a package is updated, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_package_update])]
-    public array|string $post_package_update;
+    public null|array|string $post_package_update = null;
     /** Occurs before a package has been uninstalled, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_package_uninstall])]
-    public array|string $pre_package_uninstall;
+    public null|array|string $pre_package_uninstall = null;
     /** Occurs after a package has been uninstalled, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_package_uninstall])]
-    public array|string $post_package_uninstall;
+    public null|array|string $post_package_uninstall = null;
     /** Occurs before the autoloader is dumped, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::pre_autoload_dump])]
-    public array|string $pre_autoload_dump;
+    public null|array|string $pre_autoload_dump = null;
     /** Occurs after the autoloader is dumped, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_autoload_dump])]
-    public array|string $post_autoload_dump;
+    public null|array|string $post_autoload_dump = null;
     /** Occurs after the root-package is installed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_root_package_install])]
-    public array|string $post_root_package_install;
+    public null|array|string $post_root_package_install = null;
     /** Occurs after the create-project command is executed, contains one or more Class::method callables or shell commands. */
     #[Describe(['from' => self::post_create_project_cmd])]
-    public array|string $post_create_project_cmd;
+    public null|array|string $post_create_project_cmd = null;
 }
