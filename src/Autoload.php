55aa362559f6203493a43c8142cb0b8e66687699
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:21 -0500

    doc/block Updated `src/Autoload.php`.

--- src/Autoload.php
+++ src/Autoload.php
@@ -5,7 +5,11 @@ namespace Zerotoprod\ComposerPackage;
 use Zerotoprod\DataModel\DataModel;
 use Zerotoprod\DataModel\Describe;
 
-/** Description of how the package can be autoloaded. */
+/**
+ * Description of how the package can be autoloaded.
+ *
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 class Autoload
 {
     use DataModel;
@@ -13,37 +17,43 @@ class Autoload
     /**
      * This is an object of namespaces (keys) and the directories they can be found in (values, can be arrays of paths) by the autoloader.
      *
-     * @see $psr_0
+     * @see  $psr_0
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public const psr_0 = 'psr-0';
     /**
      * This is an object of namespaces (keys) and the PSR-4 directories they can map to (values, can be arrays of paths) by the autoloader.
      *
-     * @see $psr_4
+     * @see  $psr_4
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public const psr_4 = 'psr-4';
     /**
      * This is an array of paths that contain classes to be included in the class-map generation process.
      *
-     * @see $classmap
+     * @see  $classmap
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public const classmap = 'classmap';
     /**
      * This is an array of files that are always required on every request.
      *
-     * @see $files
+     * @see  $files
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public const files = 'files';
     /**
      * This is an array of patterns to exclude from autoload classmap generation. (e.g. "exclude-from-classmap": ["/test/", "/tests/", "/Tests/"]
      *
-     * @see $exclude_from_classmap
+     * @see  $exclude_from_classmap
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public const exclude_from_classmap = 'exclude-from-classmap';
     /**
      * This is an object of namespaces (keys) and the directories they can be found in (values, can be arrays of paths) by the autoloader.
      *
      * @var null|array
+     * @link https://github.com/zero-to-prod/composer-package
      */
     #[Describe(['from' => self::psr_0])]
     public null|array $psr_0 = null;
@@ -51,6 +61,7 @@ class Autoload
      * This is an object of namespaces (keys) and the PSR-4 directories they can map to (values, can be arrays of paths) by the autoloader.
      *
      * @var null|array
+     * @link https://github.com/zero-to-prod/composer-package
      */
     #[Describe(['from' => self::psr_4])]
     public null|array $psr_4 = null;
@@ -58,18 +69,21 @@ class Autoload
      * This is an array of paths that contain classes to be included in the class-map generation process.
      *
      * @var null|array
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public null|array $classmap = null;
     /**
      * This is an array of files that are always required on every request.
      *
      * @var null|array
+     * @link https://github.com/zero-to-prod/composer-package
      */
     public null|array $files = null;
     /**
      * This is an array of patterns to exclude from autoload classmap generation. (e.g. "exclude-from-classmap": ["/test/", "/tests/", "/Tests/"]
      *
      * @var null|array
+     * @link https://github.com/zero-to-prod/composer-package
      */
     #[Describe(['from' => self::exclude_from_classmap])]
     public null|array $exclude_from_classmap = null;
