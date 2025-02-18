4a09ce8b1dc7045ec8c4f73e7e42a23878b87aec
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:22 -0500

    doc/block Updated `src/PackageRepository.php`.

--- src/PackageRepository.php
+++ src/PackageRepository.php
@@ -4,23 +4,56 @@ namespace Zerotoprod\ComposerPackage;
 
 use Zerotoprod\DataModel\DataModel;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 class PackageRepository
 {
     use DataModel;
 
-    /** @see $type */
+    /**
+     * @see  $type
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const type = 'type';
-    /** @see $canonical */
+    /**
+     * @see  $canonical
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const canonical = 'canonical';
-    /** @see $only */
+    /**
+     * @see  $only
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const only = 'only';
-    /** @see $exclude */
+    /**
+     * @see  $exclude
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const exclude = 'exclude';
-    /** @see $package */
+    /**
+     * @see  $package
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const package = 'package';
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|PackageRepositoryTypeEnum $type = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|bool $canonical = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $only = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $exclude = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $package = null;
 }
