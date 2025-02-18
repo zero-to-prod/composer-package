31734f41feb480cf7049d8c79f40a01b7cfebb3a
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:22 -0500

    doc/block Updated `src/PathRepositoryOptions.php`.

--- src/PathRepositoryOptions.php
+++ src/PathRepositoryOptions.php
@@ -4,11 +4,20 @@ namespace Zerotoprod\ComposerPackage;
 
 use Zerotoprod\DataModel\DataModel;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 class PathRepositoryOptions
 {
     use DataModel;
 
-    /** @see $symlink */
+    /**
+     * @see  $symlink
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const symlink = 'symlink';
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|bool $symlink = null;
 }
