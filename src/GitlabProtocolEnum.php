4458b27e36a4e90d17e11bb96c632ee308601a8a
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:21 -0500

    doc/block Updated `src/GitlabProtocolEnum.php`.

--- src/GitlabProtocolEnum.php
+++ src/GitlabProtocolEnum.php
@@ -2,13 +2,29 @@
 
 namespace Zerotoprod\ComposerPackage;
 
-/** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
+/**
+ * A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos.
+ *
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 enum GitlabProtocolEnum: string
 {
-    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
+    /**
+     * A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos.
+     *
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case git = 'git';
-    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
+    /**
+     * A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos.
+     *
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case http = 'http';
-    /** A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos. */
+    /**
+     * A protocol to force use of when creating a repository URL for the `source` value of the package metadata. One of `git` or `http`. By default, Composer will generate a git URL for private repositories and http one for public repos.
+     *
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case https = 'https';
 }
