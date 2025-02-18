c4ad103f3db94ba76f91cf2612f51d5d11baa532
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:22 -0500

    doc/block Updated `src/VcsRepositoryTypeEnum.php`.

--- src/VcsRepositoryTypeEnum.php
+++ src/VcsRepositoryTypeEnum.php
@@ -2,16 +2,49 @@
 
 namespace Zerotoprod\ComposerPackage;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 enum VcsRepositoryTypeEnum: string
 {
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case vcs = "vcs";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case github = "github";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case git = "git";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case gitlab = "gitlab";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case bitbucket = "bitbucket";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case git_bitbucket = "git-bitbucket";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case hg = "hg";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case fossil = "fossil";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case perforce = "perforce";
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case svn = "svn";
 }
