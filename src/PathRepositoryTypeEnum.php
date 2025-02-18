c887f57c6c1917ed9ff209a44048ee706a910092
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:22 -0500

    doc/block Updated `src/PathRepositoryTypeEnum.php`.

--- src/PathRepositoryTypeEnum.php
+++ src/PathRepositoryTypeEnum.php
@@ -2,7 +2,13 @@
 
 namespace Zerotoprod\ComposerPackage;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 enum PathRepositoryTypeEnum: string
 {
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     case path = "path";
 }
