6d6fa82234e56d2eb3cc4711bd2ba0749a1aa4d9
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:21 -0500

    doc/block Updated `src/ComposerRepository.php`.

--- src/ComposerRepository.php
+++ src/ComposerRepository.php
@@ -5,33 +5,84 @@ namespace Zerotoprod\ComposerPackage;
 use Zerotoprod\DataModel\DataModel;
 use Zerotoprod\DataModel\Describe;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 class ComposerRepository
 {
     use DataModel;
 
-    /** @see $type */
+    /**
+     * @see  $type
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const type = 'type';
-    /** @see $url */
+    /**
+     * @see  $url
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const url = 'url';
-    /** @see $canonical */
+    /**
+     * @see  $canonical
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const canonical = 'canonical';
-    /** @see $only */
+    /**
+     * @see  $only
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const only = 'only';
-    /** @see $exclude */
+    /**
+     * @see  $exclude
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const exclude = 'exclude';
-    /** @see $options */
+    /**
+     * @see  $options
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const options = 'options';
-    /** @see $allow_ssl_downgrade */
+    /**
+     * @see  $allow_ssl_downgrade
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const allow_ssl_downgrade = 'allow_ssl_downgrade';
-    /** @see $force_lazy_providers */
+    /**
+     * @see  $force_lazy_providers
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const force_lazy_providers = 'force-lazy-providers';
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|ComposerRepositoryTypeEnum $type = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|string $url = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|bool $canonical = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $only = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $exclude = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $options = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|bool $allow_ssl_downgrade = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     #[Describe(['from' => self::force_lazy_providers])]
     public null|bool $force_lazy_providers = null;
 }
