584f4a5751eb0cbf7be399106b34633c72043f63
David Smith <dasmith@zero-to-prod.com>
2025-02-17 20:33:21 -0500

    doc/block Updated `src/Dist.php`.

--- src/Dist.php
+++ src/Dist.php
@@ -4,23 +4,56 @@ namespace Zerotoprod\ComposerPackage;
 
 use Zerotoprod\DataModel\DataModel;
 
+/**
+ * @link https://github.com/zero-to-prod/composer-package
+ */
 class Dist
 {
     use DataModel;
 
-    /** @see $type */
+    /**
+     * @see  $type
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const type = 'type';
-    /** @see $url */
+    /**
+     * @see  $url
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const url = 'url';
-    /** @see $reference */
+    /**
+     * @see  $reference
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const reference = 'reference';
-    /** @see $shasum */
+    /**
+     * @see  $shasum
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const shasum = 'shasum';
-    /** @see $mirrors */
+    /**
+     * @see  $mirrors
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public const mirrors = 'mirrors';
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|string $type = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|string $url = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|string $reference = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|string $shasum = null;
+    /**
+     * @link https://github.com/zero-to-prod/composer-package
+     */
     public null|array $mirrors = null;
 }
